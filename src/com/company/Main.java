a41eb7241b96f02cb94323cb97913b7de394238d
ValentineTvardovskiy <36477034+ValentineTvardovskiy@users.noreply.github.com>
2018-09-06 13:34:34 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -56,7 +56,8 @@ public class Main {
     }
 
     public static void frequency(char ch, String str) {
-
+        
+        //int frequency = str.chars().filter(char -> char == ch).count();
         char[] chars2 = str.toCharArray();
         int frequency  = 0;
 
@@ -64,6 +65,5 @@ public class Main {
             if (ch == chars2[i]) frequency++;
         }
         System.out.println(frequency );
-
     }
 }
